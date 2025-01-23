18145a643f9e489a376d996cbfc853e96245c27e
Sky2Walker <starw8169@gmail.com>
2025-01-23 17:44:48 +0200

    OS-13 <show products & categories into shop page>

--- tests/Feature/CategoryGroupTest.php
+++ tests/Feature/CategoryGroupTest.php
@@ -24,4 +24,15 @@ class CategoryGroupTest extends TestCase
 
        $res->assertViewHas('categoriesGroups',$categoriesGroups);
    }
+
+
+    public function test_get_category_groups_in_shop_page(){
+
+        $categoriesGroups = CategoriesGroups::factory(10)->create();
+        $res=$this->get('/shop');
+        $res->assertStatus(200);
+        $res->assertViewIs('shop');
+
+        $res->assertViewHas('categoriesGroups',$categoriesGroups);
+    }
 }
